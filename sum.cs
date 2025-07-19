a54824c91449dd09279d0f5dfae1ebaa82bc7704
Sree-1211 <sreeshma1211@gmail.com>
2025-07-19 12:50:43 +0530

    Create sum.cs

---
+++ sum.cs
@@ -0,0 +1,14 @@
+using System;
+namespace MyApplication
+{
+  class Program
+  {
+      static void Main(string[] args)
+        {
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int C = A + B;
+            Console.WriteLine(C);
+        }
+  }
+}
