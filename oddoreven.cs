125364db256786ec281606aca41f1c3fcd68ca81
Sree-1211 <sreeshma1211@gmail.com>
2025-07-19 17:15:54 +0530

    Create oddoreven.cs

---
+++ oddoreven.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Solution
+{
+  public class SolutionClass
+  {
+    public static string EvenOrOdd(int number)
+    {
+      if(number%2==0)
+        return "Even"; // Code goes here ...
+      else
+        return "Odd";
+    }
+  }
+}
